eee60f74fe9c667cbffdb7fcb866d9af3c779b0b
thomas youngson <thomas youngson>
2017-03-20 16:00:54 +1300

    Changed tabs for spaces and made lines 80 chars in length

--- TimelineItem.cpp
+++ TimelineItem.cpp
@@ -4,86 +4,88 @@
 using namespace std;
 
 class TimelineItem {
-	
-	// Timeline Item data fields
-	private:
-		int id;
-		int timeline_id;
-		short int type;
-		time_t start;
-		time_t end;
-		string description;
-		unsigned int linked_int
-		unsigned int linked_items
+    
+    // Timeline Item data fields
+    private:
+        int id;
+        int timeline_id;
+        short int type;
+        time_t start;
+        time_t end;
+        string description;
+        unsigned int linked_int
+        unsigned int linked_items
 
-	// Timeline Item methods
-	public:
+    // Timeline Item methods
+    public:
 
-		// Constructor
-		TimelineItem(int, string, time_t, time_t, unsigned int, unsigned int);
+        // Constructor
+        TimelineItem(int, string, time_t, time_t, unsigned int, unsigned int);
 
-		// Getters
-		int getID();
-		int getTimelineID();
-		short int getType();
-		time_t getStartTime();
-		time_t getEndTime();
-		string getDescription();
+        // Getters
+        int getID();
+        int getTimelineID();
+        short int getType();
+        time_t getStartTime();
+        time_t getEndTime();
+        string getDescription();
 
-		// Setters
-		void setType(short int);
-		void setStartTime(time_t);
-		void setEndTime(time_t);
-		void setDescrition(string);
+        // Setters
+        void setType(short int);
+        void setStartTime(time_t);
+        void setEndTime(time_t);
+        void setDescrition(string);
 };
 
 // Constructor
-TimelineItem::TimelineItem(int type, string description, time_t start, time_t end, unsigned int linked_int = NULL, unsigned int linked_items = NULL):
-	type(type),
-	description(description),
-	start(start),
-	end(end),
-	link_int(linked_int),
-	linked_items(linked_items){}
+TimelineItem::TimelineItem(int type, string description, time_t start, 
+    time_t end, unsigned int linked_int = NULL, 
+    unsigned int linked_items = NULL):
+    type(type),
+    description(description),
+    start(start),
+    end(end),
+    link_int(linked_int),
+    linked_items(linked_items){}
 
 // Getters
 int TimelineItem::getID(){
-	return id;
+    return id;
 }
 
 int TimelineItem::getTimelineID(){
-	return timeline_id;
+    return timeline_id;
 }
 
 short int TimelineItem::getType(){
-	return type;
+    return type;
 }
 
 time_t TimelineItem::getStartTime(){
-	return start;
+    return start;
 }
 
 time_t TimelineItem::getEndTime(){
-	return end;
+    return end;
 }
 
 string TimelineItem::getDescription(){
-	return description;
+    return description;
 }
 
 // Setters
 void TimelineItem::setType(short int type){
-	this->type = type;
+    this->type = type;
 }
 
 void TimelineItem::setStartTime(time_t start){
-	this->start = start
+    this->start = start
 }
 
 void TimelineItem::setEndTime(time_t end){
-	this->end = end;
+    this->end = end;
 }
 
 void TimelineItem::setDescrition(string description){
-	this->description = description;
+    this->description = description;
 }

b30ad818346b12d87d5fad5dbe49a10a786f250a
Oliver Reid <burgerreid@gmail.com>
2017-03-30 16:22:36 +1300

    Removed contact page and added settings page

--- octagon/src/app/app.module.ts
+++ octagon/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { WeekPage } from '../pages/week/week';
-import { ContactPage } from '../pages/contact/contact';
+import { SettingsPage } from '../pages/settings/settings';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
@@ -10,7 +10,7 @@ import { TabsPage } from '../pages/tabs/tabs';
   declarations: [
     MyApp,
     WeekPage,
-    ContactPage,
+    SettingsPage,
     HomePage,
     TabsPage
   ],
@@ -21,7 +21,7 @@ import { TabsPage } from '../pages/tabs/tabs';
   entryComponents: [
     MyApp,
     WeekPage,
-    ContactPage,
+    SettingsPage,
     HomePage,
     TabsPage
   ],
--- octagon/src/pages/tabs/tabs.ts
+++ octagon/src/pages/tabs/tabs.ts
@@ -2,17 +2,18 @@ import { Component } from '@angular/core';
 
 import { HomePage } from '../home/home';
 import { WeekPage } from '../week/week';
-import { ContactPage } from '../contact/contact';
+import { SettingsPage } from '../settings/settings';
 
 @Component({
   templateUrl: 'tabs.html'
 })
+
 export class TabsPage {
   // this tells the tabs component which Pages
   // should be each tab's root Page
   tab1Root: any = HomePage;
   tab2Root: any = WeekPage;
-  tab3Root: any = ContactPage;
+  tab3Root: any = SettingsPage;
 
   constructor() {
 

808453ce77181420c8c2588432963b89f9ed64b0
Oliver Reid <burgerreid@gmail.com>
2017-04-19 16:27:04 +1200

    Fixed background image

--- octagon/src/app/app.component.ts
+++ octagon/src/app/app.component.ts
@@ -1,24 +1,25 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
-import { StatusBar, Splashscreen } from 'ionic-native';
+import { Splashscreen } from 'ionic-native';
+import { StatusBar } from '@ionic-native/status-bar';
 
 
 import { TabsPage } from '../pages/tabs/tabs';
 
 
 @Component({
-  templateUrl: 'app.html'
+  templateUrl: 'app.html',
+  providers: [StatusBar]
 })
 export class MyApp {
   rootPage = TabsPage;
 
-  constructor(platform: Platform) {
+  constructor(platform: Platform, statusBar: StatusBar) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       
-      StatusBar.overlaysWebView(false);
-      StatusBar.backgroundColorByName("white");
+      statusBar.styleLightContent();
 
       Splashscreen.hide();
     });

da3ec93ca91e762423400a8869b00a4e52fdc621
Thomas Youngson <thomas@Thomass-MacBook-Pro.local>
2017-04-24 10:51:25 +1200

    Fixed text for join page and explained each method.

--- octagon/src/pages/login/login.ts
+++ octagon/src/pages/login/login.ts
@@ -15,22 +15,26 @@ export class LoginPage {
     }
   }
 
+  /** This will stop the nav bar from showing when entering this page. */
   ionViewWillEnter() {
     if (this.tabBarElement) {
       this.tabBarElement.style.display = 'none';
     }
   }
 
+  /** Shows the nav bar when leaving the page. */
   ionViewWillLeave() {
     if (this.tabBarElement) {
       this.tabBarElement.style.display = 'flex';
     }
   }
 
+  /** Moves to the join page. */
   joinPage() {
     this.navCtrl.push(JoinPage);
   }
 
+  /** This method pops to the root of the tab then switches to the home tab. */
   homePage() {
     this.navCtrl.popToRoot();
     this.navCtrl.parent.select(0);

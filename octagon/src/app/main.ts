c737405b9605f6804fdcc2889e39f9b40618f267
Charlie Rawstorn <c.rawstorn@gmail.com>
2017-04-24 18:28:19 +1200

    Cleaned up unused import in main.ts

--- octagon/src/app/main.ts
+++ octagon/src/app/main.ts
@@ -2,7 +2,5 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
 
-import { Events } from 'ionic-angular';
-
 platformBrowserDynamic().bootstrapModule(AppModule);
 

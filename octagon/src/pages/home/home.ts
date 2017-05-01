c54fb58f34f8c9c06372b1419669b8d674c89e1a
Thomas Youngson <thomas@Thomass-MacBook-Pro.local>
2017-05-01 21:42:51 +1200

    Dates are displaying with correct numbers.

--- octagon/src/pages/home/home.ts
+++ octagon/src/pages/home/home.ts
@@ -18,4 +18,23 @@ export class HomePage {
     this.navCtrl.push(CreatePage);
   }
 
+  date = new Date();
+
+  // Create the title information
+  public title = {
+    day: this.date.getFullYear() + '-' + ('0' + (this.date.getMonth() + 1)).slice(-2) + '-' + ('0' + this.date.getDate()).slice(-2),
+  }
+
+  ionViewWillEnter() {
+    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    var now = new Date();
+
+    var day = days[now.getDay()];
+    document.getElementById('headDate').innerHTML += day;
+
+    for (var i = 0; i < 4; i++) {
+      document.getElementById("selection").innerHTML += "<h4>" + (now.getDate() + i).toString() + "/" + (now.getMonth()).toString() +  "</h4>";
+    }
+  }
+
 }

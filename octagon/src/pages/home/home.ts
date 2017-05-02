6c21d9029d8868421fd43bcbe9aba6333ba21109
Thomas Youngson <thomas@dn-wlan-170-061.sns.otago.ac.nz>
2017-05-02 14:01:20 +1200

    Week page top date header has dates ranging 5 days.

--- octagon/src/pages/home/home.ts
+++ octagon/src/pages/home/home.ts
@@ -18,13 +18,6 @@ export class HomePage {
     this.navCtrl.push(CreatePage);
   }
 
-  date = new Date();
-
-  // Create the title information.
-  public title = {
-    day: this.date.getFullYear() + '-' + ('0' + (this.date.getMonth() + 1)).slice(-2) + '-' + ('0' + this.date.getDate()).slice(-2),
-  }
-
   ionViewDidLoad() {
     var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec',];
@@ -32,11 +25,11 @@ export class HomePage {
     var day = days[now.getDay()];
 
     // Adds the day text to the page.
-    document.getElementById('headDate').innerHTML += day;
+    document.getElementById('dayDay').innerHTML += day;
 
     // Adds 5 dates to the page.
     for (var i = 0; i < 5; i++) {
-      document.getElementById("selection").innerHTML += "<h4>" + (now.getDate() + i).toString() + " " + months[(now.getMonth())] +  "</h4>";
+      document.getElementById("dayDates").innerHTML += "<h4>" + (now.getDate() + i).toString() + " " + months[(now.getMonth())] +  "</h4>";
     }
   }
 
--- octagon/src/pages/week/week.ts
+++ octagon/src/pages/week/week.ts
@@ -17,4 +17,26 @@ export class WeekPage {
     this.navCtrl.push(CreatePage);
   }
 
+  ionViewDidLoad() {
+    var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec',];
+    var now = new Date();
+    var todaysDate = (now.getDate()).toString() + " " + months[(now.getMonth())];
+
+    function addDays(dateObj, numDays) {
+      dateObj.setDate(dateObj.getDate() + numDays);
+      return dateObj;
+    }
+
+    var in5Day = addDays(new Date(), 4).getDate().toString();
+    var in5Month = months[addDays(new Date(), 4).getMonth()].toString();
+
+    // Adds the day text to the page of a 5 day range.
+    document.getElementById('weekDay').innerHTML += todaysDate + " - " + in5Day + " " + in5Month;
+
+    // Adds 5 dates to the page.
+    for (var i = 0; i < 5; i++) {
+      document.getElementById("weekDates").innerHTML += "<h4>" + (now.getDate() + i).toString() + " " + months[(now.getMonth())] + "</h4>";
+    }
+  }
+
 }

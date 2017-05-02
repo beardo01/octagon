60bed177b0b29d413474c2d989ecee2b340b0c19
Thomas Youngson <thomas@dn-wlan-170-061.sns.otago.ac.nz>
2017-05-02 13:07:20 +1200

    Dates how a lower bar and are centered.

--- octagon/src/pages/home/home.ts
+++ octagon/src/pages/home/home.ts
@@ -20,7 +20,7 @@ export class HomePage {
 
   date = new Date();
 
-  // Create the title information
+  // Create the title information.
   public title = {
     day: this.date.getFullYear() + '-' + ('0' + (this.date.getMonth() + 1)).slice(-2) + '-' + ('0' + this.date.getDate()).slice(-2),
   }
@@ -29,10 +29,12 @@ export class HomePage {
     var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec',];
     var now = new Date();
-
     var day = days[now.getDay()];
+
+    // Adds the day text to the page.
     document.getElementById('headDate').innerHTML += day;
 
+    // Adds 5 dates to the page.
     for (var i = 0; i < 5; i++) {
       document.getElementById("selection").innerHTML += "<h4>" + (now.getDate() + i).toString() + " " + months[(now.getMonth())] +  "</h4>";
     }

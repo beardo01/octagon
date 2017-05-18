a065196d451f1c753928689b3df208f38240b35a
Charlie Rawstorn <c.rawstorn@gmail.com>
2017-05-18 19:16:55 +1200

    Adding create elements to a form. Created FormBuilder. Implementing logic for form

--- octagon/src/pages/create/create.ts
+++ octagon/src/pages/create/create.ts
@@ -1,18 +1,67 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-create',
   templateUrl: 'create.html'
 })
+
 export class CreatePage {
+  createForm: FormGroup;
+  // date object
+  date = new Date();
 
-  constructor(public navCtrl: NavController) {
+  repeatFreq: string;
+  repeatFreqs: string[] = ["Never", "Daily", "Weekly", "Monthly"];
 
+  label: string;
+  labels: string [];
+
+  repeat: boolean = false;
+
+  // min and max dates for datetime picker
+  minDate: Number;
+  maxDate: Number;
+
+  repeatDate: string;
+  
+    public item = {
+    dateStarts: this.date.getFullYear() + '-' + ('0' + (this.date.getMonth() + 1)).slice(-2) + '-' + ('0' + this.date.getDate()).slice(-2),
+    dateEnds: this.date.getFullYear() + '-' + ('0' + (this.date.getMonth() + 1)).slice(-2) + '-' + ('0' + this.date.getDate()).slice(-2),
+    timeStarts: this.getTimes()[0],
+    timeEnds: this.getTimes()[1],
   }
 
-  date = new Date();
+  constructor(public navCtrl: NavController, public builder: FormBuilder) {
+    this.labels = ["Assignment", "Meeting", "Event"]; // Recieve call from plugin and pass data
+    // get freq of object (if editing existing)
+    this.repeatDate = this.item.dateEnds;
+    this.minDate = this.date.getFullYear();
+    this.maxDate = this.date.getFullYear() + 2;
+    this.getTimes();
+
+    this.createForm = this.builder.group({
+      'label' : [this.label],
+      'dateStarts' : [this.item.dateStarts],
+      'dateEnds' : [this.item.dateEnds],
+      'timeStarts': [this.item.timeStarts],
+      'timeEnds': [this.item.timeEnds],
+      'repeatFrequency' : [this.repeatFreq],
+      'repeatEndDate' : [],
+      'description': [],
+    });
+  }
+
+  add(){
+    console.log("called add()");
+    console.log(this.createForm.value);
+     
+     
+  }
+  getLabels() {
+    // Make a call to plugin to set the labels
+  }
 
   getTimes() {
     var timeStarts = this.date.getHours() + ':';
@@ -32,17 +81,8 @@ export class CreatePage {
     return new Array(timeStarts, timeEnds);
   }
 
-  // Create the TimelineItem data
-  public item = {
-    dateStarts: this.date.getFullYear() + '-' + ('0' + (this.date.getMonth() + 1)).slice(-2) + '-' + ('0' + this.date.getDate()).slice(-2),
-    dateEnds: this.date.getFullYear() + '-' + ('0' + (this.date.getMonth() + 1)).slice(-2) + '-' + ('0' + this.date.getDate()).slice(-2),
-    timeStarts: this.getTimes()[0],
-    timeEnds: this.getTimes()[1],
-  }
-
   ionViewDidLoad() {
-    console.log(this.item);
-    this.getTimes();
+    this.getLabels();
   }
 
   create() {

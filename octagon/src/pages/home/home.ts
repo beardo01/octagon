e1858a6491ba1e0588c0c5d418657a172eea9029
Thomas Youngson <thomas@dn-wlan-134-104.sns.otago.ac.nz>
2017-05-23 15:55:55 +1200

    Header dates now update with Angular 2.

--- octagon/src/pages/home/home.ts
+++ octagon/src/pages/home/home.ts
@@ -12,35 +12,40 @@ export class HomePage {
   days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
   months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-  date : Date; 
-  display_days : string []= ["0","0","0","0","0"];
+  date: Date;
+  display_days: string[] = ["0", "0", "0", "0", "0"];
   weekday_header: string;
+  selected_date: number = 0;
 
+  // Sets up dates in the header of homepage.
   constructor(public navCtrl: NavController) {
     this.date = new Date();
-    // set header
-    this.weekday_header =  this.days[this.date.getDay()];
+    // set header to the current day name from days array.
+    this.weekday_header = this.days[this.date.getDay()];
 
     // Display the next 5 days
+    // Formatted: date_number month.
     for (var i = 0; i < 5; i++) {
       this.display_days[i] = ((this.date.getDate() + i).toString() + " " + this.months[this.date.getMonth()].toString());
     }
 
   }
 
+  // Changes where the style of underlines goes for each date.
+  // Number is the button that has been clicked.
+  dateChange(newValue: number) {
+    if (this.selected_date === newValue) {
+      this.selected_date = 0;
+    }
+    else {
+      this.selected_date = newValue;
+    }
+  }
+
+  // When the add button is clicked the create page is loaded.
   createPage() {
     console.log("called");
     this.navCtrl.push(CreatePage);
-  }
-
-  dateChange(i) {
-    for (var x = 0; x < 5; x++) {
-      document.getElementById("button" + x).style.borderColor = "transparent";
-    }
-    document.getElementById("button" + i).style.borderBottom = "3px solid white";
-  }
-
-  ionViewDidLoad() {
   }
 
 }

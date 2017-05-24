56b3996f1ec3c31f18cf08779dc91f3fda56ea3b
Oliver Reid <burgerreid@gmail.com>
2017-05-24 15:02:59 +1200

    Added ODB to deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,8 @@
 echo "======== COMPILING ========\n\n"
 ../cpp/./compile.sh
 
+echo "\n\n======== ODB ========\n\n"
+../cpp/./odb.sh
+
 echo "\n\n======== MOVING TO LIVE ========"
 cp -f ../cpp/driver /var/www/html/api/

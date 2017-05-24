3426d97d730d9518d74e134ecc66e754d7b8b8d7
Thomas Youngson <thomas@dn-wlan-134-104.sns.otago.ac.nz>
2017-05-24 18:13:22 +1200

    Bubbles now sort by start time and details are writting to home page. Colours match up with timebar.

--- octagon/src/pages/home/home.ts
+++ octagon/src/pages/home/home.ts
@@ -20,12 +20,13 @@ export class HomePage {
   // ((1+((1200/1000))-(1200/1000))*40)
   // ((1+((start_time/1000)-(middle_time/1000)))*40)
   // bubble = [posistion,strart_time, end_time, type,color, location, title, description]
+  //              [0]       [1]         [2]      [3]  [4]      [5]      [6]     [7]
 
   bubbles: any[][] = [
-    ['', '2359', '1230', 'meeting', 'blue', 'Lab A', '345 Meeting', 'A meeting about cosc345'],
-    ['', '1200', '1200', 'meeting', 'red', 'Lab B', '345 Meeting', 'A meeting about cosc345'],
-    ['', '2200', '0900', 'meeting', 'yellow', 'Lab C', '345 Meeting', 'A meeting about cosc345'],
-    ['', '0000', '0700', 'meeting', 'purple', 'Lab C', '345 Meeting', 'A meeting about cosc345']
+    ['', '2359', '0100', 'meeting', 'blue', 'Lab A', '160 Meeting', 'A meeting about 160'],
+    ['', '1200', '1230', 'meeting', 'red', 'Lab B', '234 Meeting', 'A meeting about 234'],
+    ['', '2200', '2230', 'meeting', 'yellow', 'Lab C', '150 Meeting', 'A meeting about 150'],
+    ['', '0000', '0700', 'meeting', 'purple', 'Lab D', '240 Meeting', 'A meeting about 240']
   ];
 
   // Sets up dates in the header of homepage.
@@ -33,7 +34,7 @@ export class HomePage {
     this.date = new Date();
     // set header to the current day name from days array.
     this.weekday_header = this.days[this.date.getDay()];
-
+    this.bubbles.sort();
   }
 
   // Changes where the style of underlines goes for each date.
@@ -62,8 +63,12 @@ export class HomePage {
 
     // Works out the position of each bubble and writes to their array.
     for (var x = 0; x < this.bubbles.length; x++) {
-      var ml = (this.bubbles[x][1]/2359)*87;
-      this.bubbles[x][0] = ml + '%';
+      // this.bubbles[x][1] is the first time.
+      // 2359 is the heighest time on the bar.
+      // 83 is where the heighest bubble can go.
+      // +2 is the padding for start and end.
+      var margin_left = ((this.bubbles[x][1]/2359)*83) + 2;
+      this.bubbles[x][0] = margin_left + '%';
     }
   }
 

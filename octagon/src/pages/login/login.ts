9a4731e6b9a703f5b6024772d7c98096423e9291
Thomas Youngson <thomas@dn-wlan-175-153.sns.otago.ac.nz>
2017-05-27 21:42:07 +1200

    Form created for login page.

--- octagon/src/pages/login/login.ts
+++ octagon/src/pages/login/login.ts
@@ -1,18 +1,32 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { JoinPage } from '../join/join';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CreateFormValidator } from '../../validators/createForm';
+
 
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html'
 })
 export class LoginPage {
+
+  createForm: FormGroup;
+
   tabBarElement: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  email: string;
+  password: string;
+
+  constructor(public navCtrl: NavController, public builder: FormBuilder) {
     if (document.querySelector('.tabbar')) {
       this.tabBarElement = document.querySelector('.tabbar.show-tabbar');
     }
+
+    this.createForm = this.builder.group({
+      'email' : [this.email],
+      'password' : [this.password]
+    });
   }
 
   /** This will stop the nav bar from showing when entering this page. */
@@ -32,6 +46,11 @@ export class LoginPage {
   /** Moves to the join page. */
   joinPage() {
     this.navCtrl.push(JoinPage);
+  }
+
+  add(){
+    console.log("Form Submission");
+    console.log(this.createForm.value);
   }
 
   /** This method pops to the root of the tab then switches to the home tab. */

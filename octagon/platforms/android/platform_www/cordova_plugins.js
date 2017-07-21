13adec05e858c93eb8b44cb699eadc9de513f127
Thomas Youngson <thomas_youngson@hotmail.com>
2017-07-21 12:23:50 +1200

    Added android platform.

---
+++ octagon/platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,9 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-console": "1.0.5"
+};
+// BOTTOM OF METADATA
+});

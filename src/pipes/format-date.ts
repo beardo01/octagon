b182eca232e87e280909fe48bd093efdda6a46c3
Oliver Reid <burgerreid@gmail.com>
2017-09-24 18:45:49 +1300

    Fix?

---
+++ src/pipes/format-date.ts
@@ -0,0 +1,33 @@
+import { Injectable, Pipe } from '@angular/core';
+
+@Pipe({
+  name: 'formatDate'
+})
+@Injectable()
+export class FormatDate {
+
+  transform(value, args) {
+    var dateString = '';
+    var pm: boolean = false;
+    var hour = value.slice(0, 2);
+    var min = value.slice(2, value.length);
+
+    if( hour >= 12 ) {
+      pm = true;
+    }
+    if( hour > 12 ) {
+      hour = hour - 12;
+    }
+
+    dateString += hour;
+    dateString += (":" + min);
+
+    if (pm) {
+      dateString += " pm";
+    } else {
+      dateString += " am";
+    }
+    return dateString;
+  }
+
+}
---
+++ src/providers/user-local-storage.ts
@@ -0,0 +1,170 @@
+  import { Injectable } from '@angular/core';
+  import { Http } from '@angular/http';
+  import 'rxjs/add/operator/map';
+  //import { Storage } from '../providers/user-local-storage';
+  import { Storage } from '@ionic/storage';
+
+
+  // TODO: Stop turning strings into JSON objects when we don't need to
+
+
+  @Injectable()
+  export class UserLocalStorage {
+    
+  clientKey: string;
+  id: string;
+  localColours: string;
+  localLabels: string;
+  events: string; //object;
+  username: string;
+
+    constructor(public http: Http, public storage: Storage) {
+      // when constructor is called request local colours
+      this.requestLocalColours(); 
+      this.requestLocalLabels();
+      this.requestClientKey();
+      this.requestLocalID();
+    }
+
+    requestLocalID() {
+      this.storage.get('id').then((val) => {
+        this.id = JSON.parse(val);
+      })
+    }
+
+    setLocalID(id) {
+      this.id = id;
+      this.storage.set('id', JSON.stringify(id))
+    }
+
+    setLocalEvents(events) {
+      
+      this.events = events;
+      this.storage.set('events', JSON.stringify(events));
+    }
+
+    requestLocalEvents() {
+      this.storage.get('events').then((val) => {
+        this.events = JSON.parse(val);
+      })
+    }
+    
+    setUsername(username) {
+      this.username = username;
+      this.storage.set('username', JSON.stringify(username));
+    }
+
+    requestUsername() {
+      this.storage.get('username').then((val) => {
+        this.username = JSON.parse(val);
+      })
+    }
+    /**
+     * 
+     * Set the local storages values for each colour key
+     * @param colours json object
+     */
+    setLocalColours(colours){
+      this.localColours = JSON.stringify(colours);
+      this.storage.set('colours', JSON.stringify(colours));
+    }
+    /**
+    * Set the local storages values for each label key 
+    * @param labels Array of strings representing labels
+    */
+    setLocalLabels(labels){
+      this.localLabels = JSON.stringify(labels);
+      this.storage.set('labels', JSON.stringify(labels));
+    }
+
+    /**
+   * Save key value to data field and local storage
+   * @param key, value to store 
+   */
+    setClientKey(key) {
+      this.clientKey = key;
+      this.storage.set('clientKey', JSON.stringify(key));
+    }
+    
+    /**
+     * Get Colour names from local DB
+     */
+    requestLocalColours() {
+      this.storage.get('colours').then((val) => {
+        this.localColours = val;
+      })
+    }
+
+    /**
+     * Get label variables from local storage and set label datafield
+     */
+    requestLocalLabels() {
+      this.storage.get('labels').then((val) => {
+        this.localLabels = val;
+      });
+    }
+    /**
+     * request client key from local storage and set client key data field
+     * 
+    */
+    requestClientKey() {
+      this.storage.get('clientKey').then((val) =>
+        this.clientKey = JSON.parse(val));
+    }
+
+  /** 
+   * Get the colour values stored in the local storage and return an array
+   */
+    parseColoursToArray() {
+      console.log("called parseCOlours to array")
+      let colourArr = [];
+      
+      let colours = JSON.parse(this.localColours);
+
+      colourArr.push(colours.colour_one)
+      colourArr.push(colours.colour_two)
+      colourArr.push(colours.colour_three)
+      return colourArr;
+  }
+  /** 
+   * Get the label values stored in the local storage and return an array.
+   */
+    parseLabelsToArray() {
+      let labelArr = [];
+
+      let labels = JSON.parse(this.localLabels);
+      labelArr.push(labels.label_one)
+      labelArr.push(labels.label_two)
+      labelArr.push(labels.label_three)
+      return labelArr;
+  }
+  /**
+   * Takes an array of colours and creates and object we can store in local storage.
+   * 
+   * @param array colour values we want to put in local storage.
+   */
+    saveArrayOfColours(array) {
+      var jsonObj = {
+        colour_one: array[0],
+        colour_two: array[1],
+        colour_three: array[2]
+      }
+      this.setLocalColours(jsonObj);  
+    } 
+
+  /**
+   * Takes an array of labels and creates and object we can store in local storage.
+   * 
+   * @param array label values we want to put in local storage.
+   */ 
+    saveArrayOfLabels(array) {
+      var jsonObj = {
+        label_one: array[0],
+        label_two: array[1],
+        label_three: array[2]
+      }
+      this.setLocalLabels(jsonObj);
+    }
+    
+
+  }

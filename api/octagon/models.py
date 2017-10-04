ef092552e481a082fb3dff1f51228bb94b699abf
Oliver Reid <reiol787@student.otago.ac.nz>
2017-10-04 04:10:05 +0000

    Updated models to remove old dependancy.

--- api/octagon/models.py
+++ api/octagon/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django_unixdatetimefield import UnixDateTimeField
 
 
 # Run when user object is created

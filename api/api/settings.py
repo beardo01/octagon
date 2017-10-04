201c84faedc5553f0afd14a780071b749fab8af8
Oliver Reid <reiol787@student.otago.ac.nz>
2017-10-04 04:18:17 +0000

    Updated settings to get production ready

--- api/api/settings.py
+++ api/api/settings.py
@@ -20,10 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'me^leu8)7r^v--3rb)5#09j)z7+#lg2l+w)502@u7+%&2^b4g&'
+#SECRET_KEY = 'me^leu8)7r^v--3rb)5#09j)z7+#lg2l+w)502@u7+%&2^b4g&'
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'me^leu8)7r^v--3rb)5#09j)z7+#lg2l+w)502@u7+%&2^b4g&')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True)
 
 ALLOWED_HOSTS = ["*"]
 
